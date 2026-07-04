a4cb45fc0f268482f0ea2b65ab2976d4b65a2ffb
Lewechi <lewechigodsfavour@gmail.com>
2026-07-04 01:10:42 +0100

    fix: fixes

--- scripts/deploy_pool.bat
+++
@@ -1,2 +0,0 @@
-cargo build --release --target wasm32v1-none -p zendswap-pool-multi
-stellar contract deploy --wasm target/wasm32v1-none/release/zendswap_pool_multi.wasm --network testnet --source-account admin
